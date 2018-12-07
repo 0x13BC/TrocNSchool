accff5955c58a179639e315b05ab3fa7470798e6
amine <mohamed.aabid@ynov.com>
2018-12-07 09:23:58 +0100

    comit

--- app/src/main/java/fr/ydays/trocynov/trocnshcool/RegisterActivity.java
+++ app/src/main/java/fr/ydays/trocynov/trocnshcool/RegisterActivity.java
@@ -2,8 +2,11 @@ package fr.ydays.trocynov.trocnshcool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.database.
 
 public class RegisterActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Utilisateur");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
