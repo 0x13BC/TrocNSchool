6868efaced196f55b11dea39d0cb4cea07eb8c9c
Paul <cyberprime99@gmail.com>
2018-12-09 21:25:01 +0100

    Correction various issue
    Modification des activités en fragments pour simplifier les changements de pages.

---
+++ app/src/main/java/fr/ydays/trocynov/trocnshcool/MainActivity.java
@@ -0,0 +1,50 @@
+package fr.ydays.trocynov.trocnshcool;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById( R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new DonEtDemandeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.navigation_home:
+                    selectFragment = new DonEtDemandeFragment();
+                    break;
+                case R.id.navigation_profile:
+                    selectFragment = new ProfileFragment();
+                    break;
+                case R.id.navigation_post:
+                    selectFragment = new PostFragment();
+                    break;
+                case R.id.navigation_message:
+                    selectFragment = new MessagesFragment();
+                    break;
+                case R.id.navigation_notification:
+                    selectFragment = new NotificationsFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectFragment).commit();
+            return true;
+        }
+    };
+}
